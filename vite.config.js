7001f830f202a4d013f452d76ac7dbf00b8f913d
kadice <kadiceterry@gmail.com>
2025-05-17 11:26:59 +0800

    新增 GitHub Pages 自動部署流程

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/nightmarket-menu/', // 👈 這行超重要！
   plugins: [
     vue(),
     tailwindcss(),
